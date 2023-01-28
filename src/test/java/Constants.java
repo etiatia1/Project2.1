566eed8c5d99effcaee390e93d2f3864f71b20b3
User <etiatia1@gmail.com>
2023-01-28 19:19:38 +0200

    Initial commit

---
+++ src/test/java/Constants.java
@@ -0,0 +1,6 @@
+public class Constants {
+    public static final String CHROMEDRIVER_PATH="C:\\Users\\User\\Desktop\\חילוץ קבצים-אוטומציה\\" +
+            "chromedriver_win32\\chromedriver.exe";
+}
+
+
---
+++ src/test/java/Main.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+
+public class Main {
+     public static WebDriver driver;
+     private static WebDriverWait wait;
+
+     @BeforeClass
+     public static void runOnceBeforeClass() {
+          System.setProperty("webdriver.chrome.driver", Constants.CHROMEDRIVER_PATH);
+          driver = new ChromeDriver();
+          driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+          wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+          driver.get("https://buyme.co.il");
+     }
+     
+     @Test
+     public void rightOFTest() {
+          driver.findElement(By.className("notSigned")).click();
+
+     }
+     @Test
+     public void test2(){
+          driver.findElement(By.id("ember1835")).sendKeys("Etiatia1@gmail.com");
+     }
+     @Test
+     public void test3(){
+          driver.findElement(By.id("ember1842")).sendKeys("Noel135o");
+     }
+     @Test
+     public void test4(){
+          driver.findElement(By.id("ember1851")).click();
+     }
+
+//     class= "ember-view ember-text-field"
+
+}
